c5abf752f594a3159025637ecdbe234fefc7f9ce
shalu-chauhan121 <shalu.student.che24@itbhu.ac.in>
2026-06-20 17:01:18 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://interviewpilot-ai-1.onrender.com",
   credentials:true
 }));
 
